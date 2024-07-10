f7693fd3dfc2ca883f42ecf960bdbb745cd06934
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2024-07-10 16:32:19 +0000

    product item design

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,14 +1,29 @@
 // src/components/ProductCard.jsx
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import '../css/ProductCard.css';
+import UserContext from '../context/UserContext';
+import { Navigate, useNavigate } from 'react-router-dom';
 
-const ProductCard = ({ product, onAddToCart, onBuyNow }) => {
+const ProductCard = ({ product }) => {
+  const navigate = useNavigate();
+  const {info, setInfo} = useContext(UserContext);
+
+   const handleAddToCart = ()=>{
+    setInfo(product);
+    localStorage.setItem('info', JSON.stringify(info));
+    navigate("/cart");
+   }
+
+   useEffect(() => {
+    localStorage.setItem('info', JSON.stringify(info));
+  }, []);
+
   return (
     <div className="product-card">
       <img style={{height : '400px', width: 'full'}} src={product.image} alt={product.title} />
       <div className="product-details">
         <h2>{product.title}</h2>
-        <p>{product.description}</p>
+        {/* <p>{product.description}</p> */}
         <p className="price">${product.price}</p>
         <p className="category">{product.category}</p>
         <div className="rating">
@@ -16,12 +31,10 @@ const ProductCard = ({ product, onAddToCart, onBuyNow }) => {
           <span>({product.rating.count} reviews)</span>
         </div>
         <div className="buttons">
-          <button className="add-to-cart" onClick={() => onAddToCart(product)}>
-            Add to Cart
+          <button className="add-to-cart" onClick={(e) => handleAddToCart(product)}>
+            View product..
           </button>
-          <button className="buy-now" onClick={() => onBuyNow(product)}>
-            Buy Now
-          </button>
+         
         </div>
       </div>
     </div>
