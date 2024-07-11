04de7537175dd799f15f235cace3e2497d7260f7
prime_jd <ravishankartripathi2002@gmail.com>
2024-07-11 06:40:56 +0530

    product control

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,26 +1,23 @@
 import asyncHandler from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
-import { Product } from "../models/product.model.js";
+import { Feedback } from "../models/product.model.js";
 import { Cart } from "../models/cart.model.js";
 
 
 
-const Productdetails = asyncHandler( async (req, res)=>{
-    const product = await Product.find();
-    if(!product) throw new ApiError(404, "Products not found");
-    return res.json(new ApiResponse(200, "All products", product))
-})
+
 
 const addToCart = asyncHandler( async(req,res)=>{
     console.log(req.body)
-    const {id,product,price,image} = req.body;
+    const {productId,title,price,image,quantity} = req.body;
     
     const cart = await Cart.create({
-        id,
-        title,
+        productId,
+        product: title ,
         price,
-        image
+        image,
+        quantity
     })
     return res.json(new ApiResponse(200, "Product added to cart", cart))
 })
@@ -32,20 +29,41 @@ const displayCart = asyncHandler(async(req, res)=>{
 })
 
 const removeFromCart = asyncHandler(async (req, res) => {
-    const { id } = req.body;
-    if (!id) throw new ApiError(400, 'Product details not provided');
-    await Cart.deleteOne({ id });
+    const { product } = req.body;
+    if (!product) throw new ApiError(400, 'Product details not provided');
+    await Cart.deleteOne({ product });
     const cartItems = await Cart.find();
     if (cartItems.length === 0) {
       return res.json(new ApiResponse(200, 'All products removed from cart', []));
     }
     return res.json(new ApiResponse(200, 'Product removed from cart', cartItems));
   });
+
+
+  const giveFeedback = asyncHandler(async (req,res)=>{
+    const {productId,feedback,user,rating} = req.body;
+    const product = await Feedback.create({
+        productId,
+        feedback,
+        user,
+        rating
+    })
+    return res.json(new ApiResponse(200, "Feedback given", product))
+  })
+
+  const displayFeedback = asyncHandler(async(req,res)=>{
+    const {productId}= req.body;
+    const feedback = await Feedback.find({productId});
+    if(!feedback) throw new ApiError(404, "Feedback not found");
+    return res.json(new ApiResponse(200, "All feedbacks", feedback))
+  })
   
 
 export {
-    Productdetails,
+
     addToCart,
     displayCart,
-    removeFromCart
+    removeFromCart,
+    giveFeedback,
+    displayFeedback
 }
