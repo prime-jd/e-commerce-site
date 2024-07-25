be9d1e9f36f79901b50c3e960415172daff362f6
prime_jd <ravishankartripathi2002@gmail.com>
2024-07-25 09:40:03 +0530

    setup buy router

--- backend/src/router/user.router.js
+++ backend/src/router/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addToCart, displayCart ,displayFeedback,giveFeedback,removeFromCart} from "../controllers/product.controller.js";
+import { addToCart, buyFromStore, displayCart ,displayFeedback,giveFeedback,removeFromCart} from "../controllers/product.controller.js";
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -13,6 +13,7 @@ router.post('/login' ,loginUser);
 router.post('/logout',verifyJwt, logoutUser)
 router.post('/feedback', giveFeedback);
 router.post('/getfeedback', displayFeedback);
+router.post('/buyfs', buyFromStore);
 router.get('/cart', displayCart);
 router.post('/addtocart', addToCart);
 router.post('/cartremove', removeFromCart);
