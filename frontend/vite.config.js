a9750c7c427012b3202d2fc158a90538343fef5b
prime_jd <ravishankartripathi2002@gmail.com>
2024-08-04 22:50:27 +0530

    Dockerized e-shp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://server_ecom:4000', // or your API server URL
+        changeOrigin: true,
+        secure: false, // This disables SSL verification
+      },
+    },
+  },
   plugins: [react()],
-  server : {
-    proxy : {
-      '/api' : "http://localhost:4000"
-    }
-  }
 })
